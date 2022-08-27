9bcaaab4ac894542b0881f12faea4f144dc4cdad
Lorcan Mc <lorcan.jmc@gmail.com>
2022-08-27 13:09:14 +0100

    changed color scheme

--- src/components/NameBox/index.js
+++ src/components/NameBox/index.js
@@ -6,34 +6,34 @@ export default function NameBox() {
     <div className={styles.NameBox}>
       <div className={styles.container}>
         <div className={styles.letter}>
-          <div className={styles.left}></div>
-          <div className={styles.bottom}></div>
+          <div className={`${styles.left} ${styles.letterColor}`}></div>
+          <div className={`${styles.bottom} ${styles.letterColor}`}></div>
         </div>
         <div className={styles.letter}>
-          <div className={styles.left}></div>
-          <div className={styles.bottom}></div>
-          <div className={styles.top}></div>
-          <div className={styles.right}></div>
+          <div className={`${styles.left} ${styles.letterColor}`}></div>
+          <div className={`${styles.bottom} ${styles.letterColor}`}></div>
+          <div className={`${styles.top} ${styles.letterColor}`}></div>
+          <div className={`${styles.right} ${styles.letterColor}`}></div>
         </div>
         <div className={styles.letter}>
-          <div className={styles.left}></div>
-          <div className={styles.top}></div>
+          <div className={`${styles.left} ${styles.letterColor}`}></div>
+          <div className={`${styles.top} ${styles.letterColor}`}></div>
         </div>
         <div className={styles.letter}>
-          <div className={styles.left}></div>
-          <div className={styles.bottom}></div>
-          <div className={styles.top}></div>
+          <div className={`${styles.left} ${styles.letterColor}`}></div>
+          <div className={`${styles.bottom} ${styles.letterColor}`}></div>
+          <div className={`${styles.top} ${styles.letterColor}`}></div>
         </div>
         <div className={styles.letter}>
-          <div className={styles.top}></div>
-          <div className={styles.left}></div>
-          <div className={styles.right}></div>
-          <div className={styles.middle}></div>
+          <div className={`${styles.top} ${styles.letterColor}`}></div>
+          <div className={`${styles.left} ${styles.letterColor}`}></div>
+          <div className={`${styles.right} ${styles.letterColor}`}></div>
+          <div className={`${styles.middle} ${styles.letterColor}`}></div>
         </div>
         <div className={styles.letter}>
-          <div className={styles.left}></div>
-          <div className={styles.top}></div>
-          <div className={styles.right}></div>
+          <div className={`${styles.left} ${styles.letterColor}`}></div>
+          <div className={`${styles.top} ${styles.letterColor}`}></div>
+          <div className={`${styles.right} ${styles.letterColor}`}></div>
         </div>
         <div>
             <h3 className={styles.sideways}>Mccullagh</h3>
