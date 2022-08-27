48dba00f6c80575c84fddc425d2a20aa42027d3d
Lorcan Mc <lorcan.jmc@gmail.com>
2022-08-27 21:31:27 +0100

    added first display button

--- src/components/NameBox/index.js
+++ src/components/NameBox/index.js
@@ -2,6 +2,11 @@ import React from "react";
 import styles from "./NameBox.module.css";
 
 export default function NameBox() {
+
+  function handleClick() {
+    alert("I designed and made this header using CSS Animations - Lorcan")
+  }
+
   return (
     <div className={styles.NameBox}>
       <div className={styles.container}>
@@ -29,6 +34,7 @@ export default function NameBox() {
           <div className={`${styles.left} ${styles.letterColor}`}></div>
           <div className={`${styles.right} ${styles.letterColor}`}></div>
           <div className={`${styles.middle} ${styles.letterColor}`}></div>
+          <button className={`${styles.button}`} onClick={handleClick}></button>
         </div>
         <div className={styles.letter}>
           <div className={`${styles.left} ${styles.letterColor}`}></div>
---
+++ src/components/ProjectDisplay/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./ProjectDisplay.module.css";
+export default function ProjectDisplay() {
+  return (
+    <div className={styles.ProjectDisplay}>
+      <button
+        className={styles.button}
+        onClick={() => {
+          window.open("https://animle.netlify.app/", "_blank");
+        }}
+      >
+        <video
+          width="100%"
+          onMouseOver={(e) => e.target.play()}
+          src="videos/animle.mp4"
+        />
+        <div className={styles.hoverGraphic}>
+          <h2>Animle</h2>
+          <p>text</p>
+        </div>
+      </button>
+    </div>
+  );
+}
