f85b6c1136aa085877837b401e4dda91d9d9fbec
Lorcan Mc <lorcan.jmc@gmail.com>
2022-08-30 16:12:55 +0100

    adjusted whitespace

--- src/components/NameBox/index.js
+++ src/components/NameBox/index.js
@@ -9,7 +9,6 @@ export default function NameBox() {
 
   return (
     <div className={styles.NameBox}>
-      <div className={styles.container}>
         <div className={styles.letter}>
           <div className={`${styles.left} ${styles.letterColor}`}></div>
           <div className={`${styles.bottom} ${styles.letterColor}`}></div>
@@ -44,7 +43,6 @@ export default function NameBox() {
         <div>
             <h3 className={styles.sideways}>Mccullagh</h3>
         </div>
-      </div>
     </div>
   );
 }
