e34460ddc65bebacd25d8b65e7aa34835c74c40a
Lorcan Mc <lorcan.jmc@gmail.com>
2022-08-30 12:14:30 +0100

    added second project image

--- src/components/ProjectDisplay/index.js
+++ src/components/ProjectDisplay/index.js
@@ -5,22 +5,42 @@ export default function ProjectDisplay() {
     <div className={styles.ProjectSection}>
       <h1>My Projects</h1>
       <div className={styles.ProjectDisplay}>
+        <div className={styles.projectColumn}>
+          <button
+            className={styles.button}
+            onClick={() => {
+              window.open("https://animle.netlify.app/", "_blank");
+            }}
+          >
+            <video
+              width="100%"
+              src="videos/animle.mp4"
+              autoPlay
+              muted
+            />
+            <div className={styles.hoverGraphic}>
+              <h2>Animle</h2>
+            </div>
+          </button>
+        </div>
+        <div className={styles.projectColumn}>
         <button
-          className={styles.button}
-          onClick={() => {
-            window.open("https://animle.netlify.app/", "_blank");
-          }}
-        >
-          <video
-            width="100%"
-            onMouseOver={(e) => e.target.play()}
-            src="videos/animle.mp4"
-          />
-          <div className={styles.hoverGraphic}>
-            <h2>Animle</h2>
-            <p>text</p>
-          </div>
-        </button>
+            className={styles.button}
+            onClick={() => {
+              window.open("https://alotify-app.netlify.app/", "_blank");
+            }}
+          >
+            <img
+              width="100%"
+              src="/images/alotify.png"
+              
+            />
+            <div className={styles.hoverGraphic2}>
+              <h2>Alotify</h2>
+            </div>
+          </button>
+        </div>
+
       </div>
     </div>
   );
